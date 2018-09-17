a50b5f461c6a7151e392ca11d1d32753526d1963
Ilias Hossain <shimulsays@gmail.com>
2018-09-17 20:38:48 +0600

    initial commit

---
+++ src/Core/LeadsPlus.Core/Queries/IQueryExecutor.cs
@@ -0,0 +1,32 @@
+﻿namespace LeadsPlus.Core.Query
+{
+    using System.Threading.Tasks;
+    using Autofac;
+    using Autofac.Core;
+    using MediatR;
+
+    public interface IQueryExecutor
+    {
+        Task<Result> Execute<Query, Result>(Query query);
+    }
+
+    public class QueryExecutor : IQueryExecutor
+    {
+        private readonly IComponentContext container;
+        public QueryExecutor(IComponentContext container)
+        {
+            this.container = container;
+        }
+
+        public Task<Result> Execute<Query, Result>(Query query)
+        {
+            var s = container.Resolve<IMediator>();
+
+            var queryHandler = container.Resolve<IQueryHandler<Query, Result>>();// as IQueryHandler<Query, Result>;
+
+            var response = queryHandler.Handle(query);
+
+            return response;
+        }
+    }
+}
